476e1001ac0c9e7deb140278214f7650edadb460
Kinzo <sauljcervantes2@gmail.com>
2023-02-23 10:42:18 -0800

    Worked on some stuff.

--- lab-2/DesignDoc.h
+++ lab-2/DesignDoc.h
@@ -31,12 +31,27 @@ Converts a phone number from a 10-digit long long to {area code, prefix,
 lineNo}.
 Returns a StructuredPhoneNumber object {"000", "000", "0000"} if the
 input number is a negative or less than 10 digits.
-StructuredPhoneNumber phoneNumBreakdown(long long phoneNum_11);
+StructuredPhoneNumber phoneNumBreakdown(long long phoneNum_ll);
 
 Overload operator==() as a free-standing function to compare whether two
 StructuredPhoneNumber objects contain exactly the same phone number.
 */
 
 /* Algorithm Design
+  Input: Integer (phone number)
+  Output: The integer segmented into three parts: area code, prefix, lineNo
 
+  Step 1: If integer input < 0, return default StructuredPhoneNumber, else 
+          continue
+  Step 2: Convert integer input into a string variable
+  Step 3: If string variable length != 10 characters, return default 
+          StructuredPhoneNumber, else continue
+  Step 4: Pass the first 3 characters of the string variable into the 
+          areaCode field of StructuredPhoneNumber
+  Step 5: Pass the next 3 characters of the string variable into the prefix 
+          field of StructuredPhoneNumber
+  Step 6: Pass the last 4 characters of the string varaible into the lineNo 
+          field of StructuredPhoneNumber
+  Step 7: Return the constructed StructuredPhoneNumber
+  (End)
 */
---
+++ lab-2/phoneNumberBreakdown.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+
+struct StructuredPhoneNumber
+{
+  std::string areaCode = "000";
+  std::string prefix = "000";
+  std::string lineNo = "000";
+};
+
+/*
+void PrintStuct(StructuredPhoneNumber input)
+{
+  std::cout << "Area Code: " << input.areaCode << std::endl;
+  std::cout << "Prefix: " << input.prefix << std::endl;
+  std::cout << "LineNo: " << input.lineNo << std::endl;
+}
+*/
+
+StructuredPhoneNumber phoneNumBreakdown(long long phoneNum_ll);
+
+
+int main()
+{
+  StructuredPhoneNumber test = phoneNumBreakdown(9254830830);
+  //PrintStuct(test);
+  
+  return 0;
+}
+
+
+StructuredPhoneNumber phoneNumBreakdown(long long phoneNum_ll)
+{
+  StructuredPhoneNumber result;
+
+  if (phoneNum_ll < 0)
+    return result;
+
+  std::string phoneNum_string = std::to_string(phoneNum_ll);
+
+  if (phoneNum_string.length() != 10)
+    return result;
+
+  result.areaCode = phoneNum_string.substr(0, 3);
+  result.prefix = phoneNum_string.substr(3, 3);
+  result.lineNo = phoneNum_string.substr(6);
+
+  return result;
+}
