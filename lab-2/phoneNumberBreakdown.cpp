51ec9c722e4ecb6c0457c61be7dc0589834748d7
Kinzo <sauljcervantes2@gmail.com>
2023-02-23 20:32:32 -0800

    Should be finished now

--- lab-2/phoneNumberBreakdown.cpp
+++ lab-2/phoneNumberBreakdown.cpp
@@ -9,31 +9,28 @@ struct StructuredPhoneNumber
   std::string lineNo = "000";
 };
 
-/*
-void PrintStuct(StructuredPhoneNumber input)
-{
-  std::cout << "Area Code: " << input.areaCode << std::endl;
-  std::cout << "Prefix: " << input.prefix << std::endl;
-  std::cout << "LineNo: " << input.lineNo << std::endl;
-}
-*/
 
 StructuredPhoneNumber phoneNumBreakdown(long long phoneNum_ll);
 
-bool operator==(StructuredPhoneNumber lhs, StructuredPhoneNumber rhs);
+bool operator==(const StructuredPhoneNumber& lhs, const StructuredPhoneNumber& rhs);
 
-//bool test1();
-//bool test2();
-//bool test3();
+bool test1();
+bool test2();
+bool test3();
 
 bool test_operator1();
-//bool test_operator2();
+bool test_operator2();
+
 
 int main()
 {
-  StructuredPhoneNumber test = phoneNumBreakdown(9254830830);
+  std::cout << test1() << std::endl;    
+  std::cout << test2() << std::endl;    
+  std::cout << test3() << std::endl;    
+
   std::cout << test_operator1() << std::endl;
-  
+  std::cout << test_operator2() << std::endl;
+
   return 0;
 }
 
@@ -41,12 +38,12 @@ int main()
 StructuredPhoneNumber phoneNumBreakdown(long long phoneNum_ll)
 {
   StructuredPhoneNumber result;
-
+  // Negative numbers treated as "000'000'0000"
   if (phoneNum_ll < 0)
     return result;
 
   std::string phoneNum_string = std::to_string(phoneNum_ll);
-
+  // Numbers not 10 digits long treated as "000'000'0000"
   if (phoneNum_string.length() != 10)
     return result;
 
@@ -57,19 +54,71 @@ StructuredPhoneNumber phoneNumBreakdown(long long phoneNum_ll)
   return result;
 }
 
-// FIX ME
-bool operator==(StructuredPhoneNumber lhs, StructuredPhoneNumber rhs)
+
+bool operator==(const StructuredPhoneNumber& lhs, const StructuredPhoneNumber& rhs)
 {
   bool result = true;
 
-  if (!lhs.areaCode.compare(rhs.areaCode))
+  if (lhs.areaCode.compare(rhs.areaCode) != 0)
     result = false;
   
-  if (!lhs.prefix.compare(rhs.prefix))
+  if (lhs.prefix.compare(rhs.prefix) != 0)
     result = false;
-
-  if (!lhs.lineNo.compare(rhs.lineNo))
+  
+  if (lhs.lineNo.compare(rhs.lineNo) != 0)
     result = false;
 
   return result;
+}
+
+bool test1()
+{
+  // Comparing default construction of "000'000'0000", to constrution of
+  // a number not 10-digits long, also "000'000'0000"
+  StructuredPhoneNumber expected_result;
+  
+  return expected_result == phoneNumBreakdown(13);
+}
+
+bool test2()
+{
+  StructuredPhoneNumber expected_result;
+  expected_result.areaCode = "123";
+  expected_result.prefix = "456";
+  expected_result.lineNo = "7890";
+
+  return expected_result == phoneNumBreakdown(1234567890);
+}
+
+bool test3()
+{
+  // Comparing default construction of "000'000'0000", to construction of
+  // a negative number, also "000'000'0000"
+  StructuredPhoneNumber expected_result;
+
+  return expected_result == phoneNumBreakdown(-1234567890);
+}
+
+
+bool test_operator1()
+{
+  StructuredPhoneNumber lhs_test;
+  lhs_test.areaCode = "098";
+  lhs_test.prefix = "765";
+  lhs_test.lineNo = "4321";
+
+  StructuredPhoneNumber rhs_test;
+  rhs_test.areaCode = "098";
+  rhs_test.prefix = "765";
+  rhs_test.lineNo = "4321";
+
+  return lhs_test == rhs_test;
+}
+
+bool test_operator2()
+{
+  StructuredPhoneNumber lhs_test;
+  StructuredPhoneNumber rhs_test;
+
+  return lhs_test == rhs_test;
 }
